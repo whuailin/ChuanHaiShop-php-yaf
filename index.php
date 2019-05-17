3c62baef7208cf8fac41316b529f4fcad8970c81
whl <whuailin@126.com>
2019-05-17 13:47:37 +0000

    gitpd ide test

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ define('APP_PATH', __DIR__.'/protected');
 $app = new \Yaf\Application(APP_PATH . "/config/application.ini");
 $app->getDispatcher()->catchException(false);
 $app->bootstrap()->run();
+// test :add code
